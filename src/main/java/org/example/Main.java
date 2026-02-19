dc4bd8c80384a8e6928f816b1dd7f05d175f1962
ecma <agayevayhan5@gmail.com>
2026-02-19 15:57:51 +0400

    Remove not used scanner lib

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,6 @@ import java.util.List;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Scanner;
 public class Main {
     static void main(String[] args) throws IOException{
         // For the sake of simplicity and not repeating ourselves we will be using dynamic arrays
